bd00e88ce5bed841bcb4941ac588a169c0931e05
Anthony Gamon <anthonygamon@Anthonys-MBP-2.attlocal.net>
2021-01-24 21:31:57 -0600

    adding reactstrap

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Wrapper from "./components/Wrapper";
+import Navbar from "./components/Navbar";
+import Table from "./components/Table";
+import employees from "./employees.json";
+
+function App() {
+    return (
+      <Wrapper>
+       
+
+
+
+
+
+       
+      </Wrapper>
+    );
+  }
+
+
+export default App;
